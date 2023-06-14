fecfe804fd6a77d2d54f84ce8f7ac54a47d97445
guanming-zhang <guanming.zhang@physics.ox.ac.uk>
2023-06-14 16:15:32 -0400

    fixed bugs about the normalization factor for the Gaussian function

--- calculation.jl
+++ calculation.jl
@@ -25,10 +25,12 @@ set_model_params(model,T,A,B,C,Gamma)
 if input_data["iv"] == "Gaussian-profile"
     x = model.x
     y = model.y'
-    sx2 = input_data["iv_sx"]^2
-    sy2 = input_data["iv_sy"]^2
-    a = 1.0/(pi*input_data["iv_sx"]*input_data["iv_sy"])
-    rho0 = @. a*exp(-((x-0.5*x_max)/sx2)^2 -((y-0.5*y_max)/sy2)^2)
+    sx = input_data["iv_sx"]
+    sy  = input_data["iv_sy"]
+    a = 1.0/(pi*sx*sy)
+    rho0 = @. exp(-((x-0.5*x_max)/sx)^2 -((y-0.5*y_max)/sy)^2)
+    print(sum(rho0))
+    print(a)
 elseif input_data["iv"] == "random-normal"
     sr = input_data["iv_srho"]
     mu = 1.0/(x_max*y_max)
